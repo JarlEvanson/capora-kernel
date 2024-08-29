e297a8f1e8ebf3c17a922f3af3812b9050bc95a5
Jarl Evanson <evanson.jarl@gmail.com>
2024-08-29 16:03:46 -0500

    Implement `ControlledModificationCell`s

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,7 +3,10 @@
 #![no_std]
 #![no_main]
 
-#[panic_handler]
+pub mod cells;
+
+/// Handler of all panics.
+#[cfg_attr(not(test), panic_handler)]
 fn panic_handler(_: &core::panic::PanicInfo) -> ! {
     loop {
         core::hint::spin_loop()
